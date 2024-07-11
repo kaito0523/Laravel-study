8926504ab99a78e9ef654ad3ef076bf9ac33a8af
Kaito0523 <kaito.tanabe0523@gmail.com>
2024-07-11 16:04:18 +0900

    送信データの取得を設定

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.default')
+@extends('layouts.default')
 
 @section('title', 'さぁ、始めよう') 
 @section('content')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\RequestSampleController;
 use Illuminate\Support\Facades\Route;
 
 /*Route::get('/', function () {
@@ -14,3 +17,16 @@ Route::get('/hello', function () {
 });
 
 Route::get('/', fn() => view('index'));
+Route::get('/curriculum', fn() => view('curriculum'));
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
+
+Route::get('/form', [RequestSampleController::class, 'form']);
+Route::get('/query-strings', [RequestSampleController::class, 'queryStrings']);
